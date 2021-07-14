a27b36988dfb76e37b8d0e735dc2fdadc4097fb4
jdiaconu <jean.diaconu@gmail.com>
2021-07-14 15:09:06 +0200

    [JWT] Add exp claim [JWT] Add refresh token when exp is not provided [JWT] Add refresh route for refreshing the token [Global] Refactor & rename some methods

--- server/controllers/JWTController.js
+++ server/controllers/JWTController.js
@@ -11,10 +11,14 @@
 
 const JWTService = require('../services/JWTService')
 
+const crypto = require('crypto')
+
 const log4js = require('log4js')
 const logger = log4js.getLogger('jwt.controller')
 
 const Utils = require('../Utils')
+
+const _ = require('lodash')
 
 // JWT Controller
 class JWTController {
@@ -27,23 +31,147 @@ class JWTController {
     static token(req, res, next) {
         logger.info('Signing a token with JWKS')
 
-        return Utils.response(JWTService.token(req.body), res, next)
+        // Check values and generate payload
+        const values = req.body
+        const payload = {}
+
+        // Add aud
+        payload.aud = values.aud
+
+        // Check if scope is present
+        if (!values.scope) {
+            return next(Utils.createError('Invalid values', 400))
+        }
+        payload.scope = values.scope
+
+        // Get current time
+        const currentTime = Math.round(new Date().getTime() / 1000)
+
+        // Check and convert exp value
+        const parsedExp = Math.round(Number(values.exp))
+        if (!_.isUndefined(values.exp) && (_.isNaN(parsedExp) || parsedExp <= currentTime)) {
+            return next(Utils.createError('Invalid values', 400))
+        }
+        payload.exp = isNaN(parsedExp)
+            ? currentTime + Math.round(Number(process.env.JWT_DURATION))
+            : parsedExp
+
+        // Generate payload
+        payload.iss = process.env.ISSUER
+        payload.iat = currentTime
+        payload.jti = crypto.randomUUID()
+
+        // Check if needs refresh token
+        if (isNaN(parsedExp)) {
+            const refreshPayload = {
+                iat: payload.iat,
+                iss: payload.iss,
+                jti: payload.jti,
+                typ: 'Refresh',
+                claims: {
+                    scope: payload.scope,
+                    aud: payload.aud,
+                },
+            }
+
+            return Utils.response(
+                Promise.all([JWTService.create(payload), JWTService.create(refreshPayload)]).then(
+                    (values) => {
+                        return { token: values[0], jti: payload.jti, refresh_token: values[1] }
+                    }
+                ),
+                res,
+                next
+            )
+        } else {
+            return Utils.response(
+                JWTService.create(payload).then((token) => {
+                    return { token, jti: payload.jti }
+                }),
+                res,
+                next
+            )
+        }
+    }
+
+    static decode(req, _res, next) {
+        logger.info('Decoding token')
+
+        // Get and validate refresh token
+        const refreshToken = req.body.refresh_token || req.body.token
+        if (_.isUndefined(refreshToken)) {
+            return next(Utils.createError('Invalid values', 400))
+        }
+
+        return Utils.next(
+            JWTService.decode(refreshToken).then((payload) => {
+                logger.debug(`Checking payload ${JSON.stringify(payload)}`)
+
+                // Set payload
+                req.payload = payload
+                req.jti = payload.jti
+            }),
+            next
+        )
+    }
+
+    static refresh(req, res, next) {
+        logger.info('Refreshing a token')
+
+        // Check refresh type
+        if (req.payload.typ !== 'Refresh') {
+            return next(Utils.createError('Invalid refresh token', 400))
+        }
+
+        // Get current time
+        const currentTime = Math.round(new Date().getTime() / 1000)
+
+        // Create payload
+        const payload = {
+            iat: currentTime,
+            iss: req.payload.iss,
+            jti: req.jti,
+            exp: currentTime + Math.round(Number(process.env.JWT_DURATION)),
+            scope: req.payload.claims.scope,
+            aud: req.payload.claims.aud,
+        }
+
+        return Utils.response(
+            JWTService.create(payload).then((token) => {
+                return { token, jti: payload.jti }
+            }),
+            res,
+            next
+        )
     }
 
     static deny(req, res, next) {
         logger.info('Adding JTIs to the denylist')
 
-        return Utils.response(JWTService.deny(req.body.jtis), res, next)
+        // Check jtis format
+        const jtis = req.body.jtis
+        if (!_.isArray(jtis)) {
+            return next(Utils.createError('Invalid values', 400))
+        }
+
+        return Utils.response(JWTService.deny(jtis), res, next)
     }
 
-    static check(req, res, next) {
+    static checkJti(req, _res, next) {
         logger.info('Checking if jti allowed')
 
-        return Utils.response(
-            JWTService.check(req.params.jti || req.headers['x-auth-jti']),
-            res,
-            next
-        )
+        const jti = req.jti || req.params.jti || req.headers['x-auth-jti']
+        if (!Utils.isUUIDv4(jti)) {
+            return next(Utils.createError('Invalid value', 400))
+        }
+
+        return Utils.next(JWTService.check(jti), next)
+    }
+
+    static jtiOk(_req, res, _next) {
+        logger.info('Sending jti allowed')
+
+        res.send(200)
     }
 }
 
